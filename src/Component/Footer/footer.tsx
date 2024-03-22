46976059c18e985056dbd7778d75fd6ae37d8dea
Ivan-ISS <vanya5@inbox.ru>
2024-03-22 19:14:05 +0500

    project_kanban-board: added display of the number of active and completed tasks

--- src/Component/Footer/footer.tsx
+++ src/Component/Footer/footer.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 import style from './footer.module.scss';
 import Inscription from "../Common/Inscription/inscription";
 import Button from "../Common/Button/button";
+import toArrayNumBlocks from '../../utils/to-array-helper';
 
 const Footer = () => {
+    const dataFromLocalStorage = localStorage.getItem('tasksListJSON');
+    const pars = dataFromLocalStorage !== null ? JSON.parse(dataFromLocalStorage) : [];
+    const arrayNumBlocks = toArrayNumBlocks(pars)
+
+    const valueFinish = 4
+    const valueBacklog = 1
+    const countFinish = arrayNumBlocks.filter(item => item === valueFinish).length
+    const countBacklog = arrayNumBlocks.filter(item => item === valueBacklog).length
 
     const handleClickBtn = () => {
         localStorage.removeItem('tasksListJSON')
@@ -13,8 +22,8 @@ const Footer = () => {
    return (
         <footer className={style.footer}>
             <div className={style.taskCounter}>
-                <Inscription name={"Active tasks"} meaningFirst={5} />
-                <Inscription name={"Finished tasks"} meaningFirst={10} />
+                <Inscription name={"Active tasks"} meaningFirst={countBacklog} />
+                <Inscription name={"Finished tasks"} meaningFirst={countFinish} />
             </div>
                 <Button handleClickBtn={handleClickBtn} name={"Clear board"}/>
                 <Inscription name={"Kanban board by"} meaningFirst={"ISS"} meaningSecond={2024}/>

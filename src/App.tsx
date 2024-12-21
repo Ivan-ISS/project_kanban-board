9141c8e9a4765f4e883cd3aac9a7fbbbfe357ee7
Ivan-ISS <vanya5@inbox.ru>
2024-12-21 12:43:04 +0500

    project_kanban-board__refactor: fonts are connected

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-function App() {
+import { FunctionComponent } from 'react';
+
+const App: FunctionComponent = () => {
     return (
         <div>
             <div>Прива</div>
         </div>
     );
-}
+};
 
 export default App;
